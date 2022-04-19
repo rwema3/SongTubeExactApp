0c3c673cbff78f1c3f2c69e7eb53acc834bd4bfb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 16:43:52 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -98,7 +98,7 @@ class _LibState extends State<Lib> {
         String intent = await NativeMethod.handleIntent();
         if (intent == null) return;
         _handleIntent(intent);
-        return
+        retur
       
     
     
