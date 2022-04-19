34f43e860b739f251c6f2c66a14c2c9db941ac0f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 20:11:27 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -130,7 +130,7 @@ class _LibState extends State<Lib> {
     }
     if (config.showDownloadFixDialog && config.preferences.sdkInt >= 30) {
       bottomSheets.add(DownloadFixSheet());
-      config.showDownloadFixDialog = 
+      config.showDownloadFixDialog = f
     
        
      
