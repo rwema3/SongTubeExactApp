5089423ac598dd807f45c765b522a54436293dfe
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 17:33:12 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -84,7 +84,7 @@ class _LibState extends State<Lib> {
             showModalBottomSheet(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15
+                  topLeft: Radius.circular(1
                   
                 
               
