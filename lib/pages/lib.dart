5aa423d10a9b1d44a3259916cb4649740b10533b
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 20:40:00 +0200

    } else if (screenIndex == 1) {

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -280,6 +280,7 @@ class _LibState extends State<Lib> {
   Widget _currentScreen(screenIndex) {
     if (screenIndex == 0) {
       return HomeScreen();
+    } else if (screenIndex == 1) {
   
   
  
