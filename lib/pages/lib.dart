23b1dfad4aa564ed8f8431007760c83add8c6533
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 16:58:12 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -95,7 +95,7 @@ class _LibState extends State<Lib> {
             );
           }
         }
-        String intent = await NativeMethod.handleInten
+        String intent = await NativeMethod.handleInt
            
         
         
