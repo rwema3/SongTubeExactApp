6a9ed08f2331c4720b37064800918083a37833a1
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 20:30:49 +0200

    StyledBottomSheetList(children: bottomSheets)

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -146,6 +146,7 @@ class _LibState extends State<Lib> {
         ),
         context: context,
         builder: (context) => Wrap(children: [
+          StyledBottomSheetList(children: bottomSheets)
 
   
  
