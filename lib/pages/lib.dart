9d4c8f085b37f8674960dd17a24adfab549521d7
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 09:44:06 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -350,5 +350,4 @@ class _LibState extends State<Lib> {
           SystemChrome.setSystemUIOverlayStyle(
             SystemUiOverlayStyle(
               statusBarIconBrightness: prefs.enablePlayerBlurBackground ? mediaProvider.textColor == Colors.black
-                ? Brightness.dark : Brightness.light : iconColor,
  
