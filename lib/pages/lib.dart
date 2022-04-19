2d7c161386ecd8e4c050b8706e3362b412f61ba9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 20:48:03 +0200

    } else {

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -372,4 +372,5 @@ class _LibState extends State<Lib> {
     VideoPageProvider pageProvider = Provider.of<VideoPageProvider>(context);
     if (pageProvider.infoItem != null) {
       return pageProvider.fwController;
+    } else {
  
