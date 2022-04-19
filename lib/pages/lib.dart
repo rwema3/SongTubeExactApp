2e5af0dea47871d7247be2243b97dbae3b5f0714
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 11:11:37 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -155,7 +155,6 @@ class _LibState extends State<Lib> {
   void _checkForUpdates() {
     PackageInfo.fromPlatform().then((android) {
       double appVersion = double
-        .parse(android.version.replaceRange(3, 5, ""));
 
   
  
