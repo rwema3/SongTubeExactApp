bfbf8bc55abeba58a8eefb260d30e7b3d47ed0de
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 09:45:27 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -315,7 +315,6 @@ class _LibState extends State<Lib> {
     } else if (AudioService?.currentMediaItem != null) {
       return _floatingMusicWidgetConfig();
     } else {
-      return FloatingWidgetConfig(
  
  
  
