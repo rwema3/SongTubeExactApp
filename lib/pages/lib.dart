17a240a6bea2fca625bfd3a2deea26d34184476e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 18:04:14 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -101,7 +101,7 @@ class _LibState extends State<Lib> {
         return;
       })
     );
-    Provider.of<MediaProvider>(context, listen: false).loadSongList().th
+    Provider.of<MediaProvider>(context, listen: false).loadSongList().the
       
     
   
