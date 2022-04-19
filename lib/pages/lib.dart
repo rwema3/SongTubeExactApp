fa31eddf6b8deb72a4ac05f61ed57f9c5b6d6644
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 11:10:27 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -179,7 +179,6 @@ class _LibState extends State<Lib> {
 
   void _handleIntent(String intent) async {
     String streamId = await YoutubeId.getIdFromStreamUrl(intent);
-    String playlistId = await YoutubeId.getIdFromPlaylistUrl(intent);
 
   
  
