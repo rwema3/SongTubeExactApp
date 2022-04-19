d2894842d0e6ecb79f8b25aaa1de31e073b10986
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 20:44:06 +0200

    if (pageProvider.infoItem != null) {

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -308,6 +308,7 @@ class _LibState extends State<Lib> {
   
   FloatingWidgetConfig _currentFloatingWidetConfig() {
     VideoPageProvider pageProvider = Provider.of<VideoPageProvider>(context);
+    if (pageProvider.infoItem != null) {
  
  
  
