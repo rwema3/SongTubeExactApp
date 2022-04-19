184847155c7ceee612b1c0b27df78e5cb32dcd21
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 17:38:01 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -41,8 +41,6 @@ import 'package:songtube/ui/sheets/joinTelegram.dart';
 import 'package:songtube/ui/dialogs/loadingDialog.dart';
 import 'package:songtube/ui/sheets/disclaimer.dart';
 import 'package:songtube/ui/sheets/downloadFix.dart';
-import 'package:songtube/ui/internal/lifecycleEvents.dart';
-
 
                  
                    
