4f0c41866932e54870d34c38aa4ac95337aeed59
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 20:33:45 +0200

    .infoItem = video.toStreamInfoItem();

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -187,6 +187,7 @@ class _LibState extends State<Lib> {
       );
       YoutubeVideo video = await VideoExtractor.getStream(intent);
       Provider.of<VideoPageProvider>(context, listen: false)
+        .infoItem = video.toStreamInfoItem();
 
   
  
