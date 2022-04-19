7f881f47e4a017fad2773110c00d1b7a315b6f22
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 09:43:59 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -352,5 +352,4 @@ class _LibState extends State<Lib> {
               statusBarIconBrightness: prefs.enablePlayerBlurBackground ? mediaProvider.textColor == Colors.black
                 ? Brightness.dark : Brightness.light : iconColor,
               systemNavigationBarIconBrightness: prefs.enablePlayerBlurBackground ? sdkInt >= 30 ?
-                mediaProvider.textColor == Colors.black ? Brightness.dark : Brightness.light : null : iconColor,
  
