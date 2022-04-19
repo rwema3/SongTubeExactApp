1808eee77b5bed29b61044d5091cbf968cce7e6c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 20:28:37 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -139,7 +139,7 @@ class _LibState extends State<Lib> {
         enableDrag: false,
         isScrollControlled: true,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.on
+          borderRadius: BorderRadius.onl
              
         
   
