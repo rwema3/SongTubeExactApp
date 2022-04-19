429bf3a6c7d9d2d8a58ade5d59a53bf19a8f34b8
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 09:44:41 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -338,5 +338,4 @@ class _LibState extends State<Lib> {
   FloatingWidgetConfig _floatingMusicWidgetConfig() {
     MediaProvider mediaProvider = Provider.of<MediaProvider>(context);
     PreferencesProvider prefs = Provider.of<PreferencesProvider>(context);
-    ConfigurationProvider config = Provider.of<ConfigurationProvider>(context, listen: false);
  
