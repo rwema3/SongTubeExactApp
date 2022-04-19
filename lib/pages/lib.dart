76465903f6826bfd76dc54a26657623f5a62c1db
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 09:39:58 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -358,5 +358,4 @@ class _LibState extends State<Lib> {
         } else if (position < 0.95) {
           SystemChrome.setSystemUIOverlayStyle(
             SystemUiOverlayStyle(
-              statusBarIconBrightness: iconColor,
  
