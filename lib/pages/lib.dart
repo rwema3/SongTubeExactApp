aab154ba25239abb72e489385c6357dfc9398b45
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 20:45:46 +0200

    return FloatingWidgetConfig(

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -339,4 +339,5 @@ class _LibState extends State<Lib> {
     MediaProvider mediaProvider = Provider.of<MediaProvider>(context);
     PreferencesProvider prefs = Provider.of<PreferencesProvider>(context);
     ConfigurationProvider config = Provider.of<ConfigurationProvider>(context, listen: false);
+    return FloatingWidgetConfig(
  
