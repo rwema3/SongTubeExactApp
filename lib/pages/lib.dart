d510ccab801625d7aada96830d1156213890e56c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 13:55:27 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -121,8 +121,8 @@ class _LibState extends State<Lib> {
   }
 
   void _showSheets() {
-    ConfigurationProvider config = Provider.of<ConfigurationProvider>(context, listen: false);
-         
+    ConfigurationProvider config = Provider.of<ConfigurationProvider>(context, listen: false)
+       
        
      
       
