aa57679e4063ad6c75b93004fdc52061690efb55
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 11:11:18 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -161,7 +161,6 @@ class _LibState extends State<Lib> {
           .split("+").first.trim().replaceRange(3, 5, ""));
         if (appVersion < newVersion) {
           // Show the user an Update is available
-          showModalBottomSheet(
 
   
  
