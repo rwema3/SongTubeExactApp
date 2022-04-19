0870239830c3b5f9a765167535b6ed66ea647295
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-19 17:22:55 +0200

    Update lib.dart

--- lib/pages/lib.dart
+++ lib/pages/lib.dart
@@ -89,7 +89,7 @@ class _LibState extends State<Lib> {
                 )
               ),
               context: context,
-              builder: (_) => Wrap(c
+              builder: (_) => Wrap(
                 
               
             
