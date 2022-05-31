e1bcffcfe665b263b0171931dca69bc9555a331e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 11:25:38 +0200

    Update about.dart

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -10,7 +10,6 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
                 
-                    fontWeight: FontWeight.w600,
                     fontFamily: 'Product Sans'
                   ),
                 ),
