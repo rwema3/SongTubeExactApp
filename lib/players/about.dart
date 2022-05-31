8563e81242fa6c224f7b08cf77659bfb9a73086c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:23:40 +0200

    fontWeight: FontWeight.w600,

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -51,6 +51,7 @@ class AboutPage extends StatelessWidget {
                 Text(
                   "Airis Team",
                   style: TextStyle(
+                    fontWeight: FontWeight.w600,
                     fontFamily: 'Product Sans'
                   ),
                 ),
