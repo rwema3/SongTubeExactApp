7259c56571520a163742ef1c2638b6f2eb5d0876
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:20:27 +0200

    EvaIcons.emailOutline,

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -45,7 +45,7 @@ class AboutPage extends StatelessWidget {
                 launch('mailto:artx4dev@gmail.com');
               },
               leading: Icon(
-                
+                EvaIcons.emailOutline,
                 color: Colors.red,
                 size: 28,
               ),
