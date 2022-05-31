e8189712d7712ca00f04856311050a4c4b2fba86
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:20:31 +0200

    color: Color.fromARGB(255, 0, 10, 28),

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -23,6 +23,7 @@ class AboutPage extends StatelessWidget {
                   ),
                   padding: EdgeInsets.all(8),
                   child: Container(
+                      color: Color.fromARGB(255, 0, 10, 28),
                       borderRadius: BorderRadius.circular(100)
                     ),
                     padding: EdgeInsets.all(12),
