0d3b8b49b24c7b4771ffb1412b8a55f63ffb4987
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:28:53 +0200

    fontFamily: 'Product Sans',

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -198,3 +198,4 @@ class AboutPage extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
+                  fontFamily: 'Product Sans',
