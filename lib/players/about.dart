439d8844ef5c2fe115e6abca94280d3ff649257a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:22:31 +0200

    style: TextStyle(

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -10,6 +10,7 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
                 
+                  style: TextStyle(
                     color: Theme.of(context).textTheme.bodyText1.color,
                     fontSize: 18,
                     fontWeight: FontWeight.w600,
