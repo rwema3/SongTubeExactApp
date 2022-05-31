1ed9cf33edb8a5f975ee2720476d7a6267cd82e1
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:26:46 +0200

    body: Padding

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -43,7 +43,8 @@ class AboutPage extends StatelessWidget {
           )
         ],
       ),
-         
+      body: Padding(
+          
                   padding: EdgeInsets.all(8),
                   child: Container(
                     decoration: BoxDecoration(
