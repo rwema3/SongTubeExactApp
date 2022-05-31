90e96bcc80dec7b2f8c03c7c752143df85365ec3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:18:55 +0200

    import 'package:flutter/material.dart';

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:songtube/provider/configurationProvider.dart';
