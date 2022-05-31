b8c5bd71f1c7f64c22ce7cea67657b550501d417
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:15:35 +0200

    "artx4dev@gmail.com",

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -5,5 +5,7 @@
                 
               ),
               subtitle: Text(
+                "artx4dev@gmail.com",
+              
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
