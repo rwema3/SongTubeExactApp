f0a861cbc2a8de8c034606587dbfa02a3f23551f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:26:58 +0200

    child: ListView

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -45,7 +45,8 @@ class AboutPage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.only(left: 40, right: 40),
-           
+        child: ListView(
+             
                   padding: EdgeInsets.all(8),
                   child: Container(
                     decoration: BoxDecoration(
