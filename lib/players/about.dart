ac5fea2ab6f577ff765a9f979d0386b3b9042593
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:19:18 +0200

    width: MediaQuery.of(context).size.width*0.09,

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -56,6 +56,7 @@ class AboutPage extends StatelessWidget {
                 style: TextStyle(
               },
               leading: Image.asset(
+                width: MediaQuery.of(context).size.width*0.09,
               ),
               title: Text(
                 "Airis Email",
