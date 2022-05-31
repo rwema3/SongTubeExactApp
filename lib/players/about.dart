8be76153e9fd3345463c3385912d8e9d267f11f3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:25:31 +0200

    style: TextStyle(

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -114,6 +114,7 @@ class AboutPage extends StatelessWidget {
               "We are a Development Team dedicated to professional software "
               "solutions.\n\nContact us to know more about the best Web "
               "and Mobile services we can offer you.",
+              style: TextStyle(
            
             // Artx Email
             ListTile(
