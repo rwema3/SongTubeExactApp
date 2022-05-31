7022169ba217ecc7a4a3065447a238455432599e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:18:04 +0200

    child: Container(

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -16,6 +16,7 @@ class AboutPage extends StatelessWidget {
                   style: TextStyle(
                   ),
                   padding: EdgeInsets.all(8),
+                  child: Container(
                       width: MediaQuery.of(context).size.width*0.15,
                     ),
                   ),
