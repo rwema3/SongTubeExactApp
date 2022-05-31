f418772d8c5f44b2107a2fd1915e8e0e45ca4a26
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:26:50 +0200

    padding: EdgeInsets.only(left: 40, right: 40),

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -44,7 +44,8 @@ class AboutPage extends StatelessWidget {
         ],
       ),
       body: Padding(
-          
+        padding: EdgeInsets.only(left: 40, right: 40),
+           
                   padding: EdgeInsets.all(8),
                   child: Container(
                     decoration: BoxDecoration(
