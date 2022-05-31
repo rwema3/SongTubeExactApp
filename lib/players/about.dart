60d1bcc6b88d64563cc5dc150799d767345958b8
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 11:26:03 +0200

    Update about.dart

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -17,9 +17,6 @@ class AboutPage extends StatelessWidget {
             ),
             SizedBox(height: 16),
             RichText(
-                ),
-                children: [
-                  TextSpan(
                     text: "It's an Open Source app for media downloading "
                       "or streaming purposes\n\nMeant to be a beautiful, "
                       "fast and functional alternative to other Youtube "
