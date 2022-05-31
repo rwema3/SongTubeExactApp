f3efec3885ba8e62ad81008b26025c20bc845589
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 11:32:17 +0200

    Update about.dart

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -1,6 +1,5 @@
            
                 launch('mailto:artx4dev@gmail.com');
               },
-              leading: Icon(
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
