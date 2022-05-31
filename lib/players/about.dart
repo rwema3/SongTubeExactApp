b1f271fbc07d482a404573985d801bd506ecb5c6
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 11:23:14 +0200

    Update about.dart

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -51,7 +51,6 @@ class AboutPage extends StatelessWidget {
                 Text(
                   "Airis Team",
                   style: TextStyle(
-                    fontFamily: 'Product Sans'
                   ),
                 ),
                 SizedBox(width: 32),
