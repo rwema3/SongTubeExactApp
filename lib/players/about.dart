53d6d3c89d813866ee08742b157b2112ecb9d09c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-31 14:24:05 +0200

    decoration: BoxDecoration(

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -15,6 +15,7 @@ class AboutPage extends StatelessWidget {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         title: Text(
           "About us",
+                    decoration: BoxDecoration(
                       color: Colors.white,
                     ),
                 
