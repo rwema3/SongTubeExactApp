e51e1f630eeb862d2b9ba927fdb5c167fb2bce74
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-01 10:14:48 +0200

    Update about.dart

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -175,7 +175,7 @@ class AboutPage extends StatelessWidget {
                   color: Theme.of(context).textTheme.bodyText1.color
                 )
               ),
-            
+            S
           ],
         ),
       ),
