d185b612b58946366b581d99d6a08d4ed1644c6c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-01 10:15:23 +0200

    Update about.dart

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -190,6 +190,7 @@ class AboutPage extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
+                  fontFamily: 'Product Sans',
             
           ],
         ),
