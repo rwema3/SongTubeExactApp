bcbc0d6ba796651b267d2c243f163e56358342fb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-01 10:14:26 +0200

    Update about.dart

--- lib/players/about.dart
+++ lib/players/about.dart
@@ -167,6 +167,7 @@ class AboutPage extends StatelessWidget {
               ),
               title: Text(
                 "Email",
+                textAlign: TextAlign.start,
             
           ],
         ),
